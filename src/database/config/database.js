4b3881594dd835da34621c97fb195de9f72a447d
Murilo Avila <murilo.avila068@gmail.com>
2020-08-25 11:17:46 -0300

    protegendo o banco de dados

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false, useCreateIndex:true});
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false, useCreateIndex:true, user: process.env.MONGO_USERNAME, pass: process.env.MONGO_PASS});
 mongoose.Promise = global.Promise;
 
 export default mongoose; 
